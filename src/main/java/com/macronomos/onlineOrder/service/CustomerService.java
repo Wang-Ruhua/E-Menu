2990d14e0160ca776191b95a2249d7634e982d92
Ruhua Wang <ruhuawan@usc.edu>
2023-03-16 17:09:07 -0700

    first commit

---
+++ src/main/java/com/macronomos/onlineOrder/service/CustomerService.java
@@ -0,0 +1,30 @@
+package com.macronomos.onlineOrder.service;
+
+import com.macronomos.onlineOrder.dao.CustomerDao;
+import com.macronomos.onlineOrder.entity.Cart;
+import com.macronomos.onlineOrder.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private CustomerDao customerDao;
+
+    @Autowired
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public void signUp(Customer customer) {
+        Cart cart = new Cart();
+        customer.setCart(cart);
+        customer.setEnabled(true);
+        customerDao.signUp(customer);
+    }
+
+    public Customer getCustomer(String email) {
+        return customerDao.getCustomer(email);
+    }
+}
+
